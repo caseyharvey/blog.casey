0dbecf57699c84a325cdddcf033d77b284469b0b
caseyharvey <caseyharvey1@gmail.com>
2020-03-31 16:30:36 +1100

    Change icon name

--- src/components/ThemeToggle.js
+++ src/components/ThemeToggle.js
@@ -1,12 +1,12 @@
 import { ThemeToggler } from "gatsby-plugin-dark-mode"
 import React from "react"
-import Icon from "../../content/assets/dark-mode-icon.svg"
+import ToggleIcon from "../../content/assets/dark-mode-icon.svg"
 
 function ThemeToggle() {
   return (
     <ThemeToggler>
       {({ theme, toggleTheme }) => (
-        <Icon
+        <ToggleIcon
           className="light-dark-btn"
           onClick={() => toggleTheme(theme === "dark" ? "light" : "dark")}
         />
